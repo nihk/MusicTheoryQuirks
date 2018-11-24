37e029c0151d3f0f563b8b04bf075d264c1d6ca2
Nick Rose <nicholas.rose@cbc.ca>
2018-11-24 15:44:23 -0500

    More idiomatic kotlin for when statement

--- src/main/kotlin/Accidental.kt
+++ src/main/kotlin/Accidental.kt
@@ -2,9 +2,11 @@ import kotlin.math.abs
 
 data class Accidental(val modifier: Int) {
 
-    override fun toString() = when {
-        modifier < 0 -> "b".repeat(abs(modifier))
-        modifier > 0 -> "#".repeat(modifier)
-        else -> ""  // Implicit natural
+    override fun toString() = modifier.let {
+        when {
+            it < 0 -> "b".repeat(abs(it))
+            it > 0 -> "#".repeat(it)
+            else -> ""  // Implicit natural
+        }
     }
 }
