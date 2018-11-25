0374379b7c2c865394ad657d17f38e50d12d9ba4
Nick Rose <nicholas.rose@cbc.ca>
2018-11-25 06:05:00 -0500

    Change flat/sharp symbol to real unicode types

--- src/main/kotlin/Accidental.kt
+++ src/main/kotlin/Accidental.kt
@@ -4,8 +4,8 @@ data class Accidental(val modifier: Int) {
 
     override fun toString() = modifier.let {
         when {
-            it < 0 -> "b".repeat(abs(it))
-            it > 0 -> "#".repeat(it)
+            it < 0 -> "♭".repeat(abs(it))
+            it > 0 -> "♯".repeat(it)
             else -> ""  // Implicit natural
         }
     }
