bbc0c8efe8a31c9d8a40a8b379578ded450bb3a9
Nick Rose <nicholas.rose@cbc.ca>
2018-11-25 08:10:31 -0500

    Further code golf Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 fun main(args: Array<String>) {
-    val c4 = Pitch(PitchClass(PitchLetter.C), 4)
-
-    c4.toCollection(Interval.chromaticScale).forEach { pitch ->
-        println("majorScale:           ${pitch.toCollection(Interval.majorScale)}")
-        println("naturalMinorScale:    ${pitch.toCollection(Interval.naturalMinorScale)}")
-        println("majorPentatonicScale: ${pitch.toCollection(Interval.majorPentatonicScale)}")
-        println("minorPentatonicScale: ${pitch.toCollection(Interval.minorPentatonicScale)}")
-        println("wholeToneScale:       ${pitch.toCollection(Interval.wholeToneScale)}")
-        println("majorOctatonicScale:  ${pitch.toCollection(Interval.majorOctatonicScale)}")
-        println("minorOctatonicScale:  ${pitch.toCollection(Interval.minorOctatonicScale)}")
-        println("circleOfFifths:       ${pitch.toCollection(Interval.circleOfFifths)}")
-        println("majorTriad:           ${pitch.toCollection(Interval.majorTriad)}")
-        println("minorTriad:           ${pitch.toCollection(Interval.minorTriad)}\n")
-    }
+    Pitch(PitchClass(PitchLetter.C), 4)
+        .toCollection(Interval.chromaticScale).forEach { pitch ->
+            println("majorScale:           ${pitch.toCollection(Interval.majorScale)}")
+            println("naturalMinorScale:    ${pitch.toCollection(Interval.naturalMinorScale)}")
+            println("majorPentatonicScale: ${pitch.toCollection(Interval.majorPentatonicScale)}")
+            println("minorPentatonicScale: ${pitch.toCollection(Interval.minorPentatonicScale)}")
+            println("wholeToneScale:       ${pitch.toCollection(Interval.wholeToneScale)}")
+            println("majorOctatonicScale:  ${pitch.toCollection(Interval.majorOctatonicScale)}")
+            println("minorOctatonicScale:  ${pitch.toCollection(Interval.minorOctatonicScale)}")
+            println("circleOfFifths:       ${pitch.toCollection(Interval.circleOfFifths)}")
+            println("majorTriad:           ${pitch.toCollection(Interval.majorTriad)}")
+            println("minorTriad:           ${pitch.toCollection(Interval.minorTriad)}\n")
+        }
 }
